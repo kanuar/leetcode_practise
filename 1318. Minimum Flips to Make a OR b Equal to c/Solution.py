659868fad07d7f9995a86bddb76144abb26ecaea
raunak kodwani <68312266+kanuar@users.noreply.github.com>
2023-06-08 10:57:44 +0530

    Update Solution.py

--- 1318. Minimum Flips to Make a OR b Equal to c/Solution.py
+++ 1318. Minimum Flips to Make a OR b Equal to c/Solution.py
@@ -1,25 +1,28 @@
-% error in test case with values [5,2,8] --> solution points at a result of 4 bit flips but upon manual calculation it is noted that 1 bit flip is enough 
-from math import *
-
 class Solution:
-    def minFlips(self, a, b, c):
-        l=[a,b,c]
-        l.sort()
-        if(l[0]+l[1]==l[2]) and l[0]!=a:
-            return l[0]
-        if(a!=b and b!=c and a!=c):
-            s=c-(b+a)
-            return abs(s)
-        else:
-            if(a==b==c):
-                return 0
-            if(a==b or b==c or c==a):
-                return 1
+    def minFlips(self, a,b,c):
+        k=len(bin(max(a,b,c)))-2
+
+        a=bin(a)[2:]
+        b=bin(b)[2:]
+        c=bin(c)[2:]
+        ka=k-len(a)
+        kb=k-len(b)
+        kc=k-len(c)
+        a='0'*ka+a
+        b='0'*kb+b
+        c='0'*kc+c
+        count=0
+        for i in range(k):
+            x=int(a[i])
+            y=int(b[i])
+            z=int(c[i])
+            if(x|y!=z):
+                count+=1
+                if(x==y==1):
+                    count+=1
+                    pass
+                pass
+            pass
+        return count
     pass
 
-
-
-
-k=Solution()
-t=[[1,2,3],[4,5,9],[2,4,7],[7,7,7],[1,1,2],[1,2,1],[2,6,5],[5,3,8]]
-v=[k.minFlips(i[0],i[1],i[2]) for i in t]
